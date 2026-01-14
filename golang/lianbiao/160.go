9e2a8db46be8824ab5413cfeb493ee0c16a07387
enjoykanyu <creatkanyu@outlook.com>
2026-01-14 23:41:30 +0800

    map解决

--- golang/lianbiao/160.go
+++ golang/lianbiao/160.go
@@ -22,3 +22,21 @@ func getIntersectionNode(headA, headB *ListNode) *ListNode {
 	}
 	return nil
 }
+
+// map解决
+func getIntersectionNode1(headA, headB *ListNode) *ListNode {
+	listA := headA
+	listB := headB
+	listNodeMap := make(map[*ListNode]string)
+	for listA != nil {
+		listNodeMap[listA] = " "
+		listA = listA.Next
+	}
+	for listB != nil {
+		if listNodeMap[listB] == " " {
+			return listB
+		}
+		listB = listB.Next
+	}
+	return nil
+}

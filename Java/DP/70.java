3e4856dc74c6049ad648294cee18ab935e66fdd8
enjoykanyu <creatkanyu@outlook.com>
2026-01-20 23:39:53 +0800

    动态规划爬楼梯

---
+++ Java/DP/70.java
@@ -0,0 +1,19 @@
+class Solution {
+//    public int climbStairs(int n) {
+//        if (n<=1){
+//            return 1;
+//        }else{
+//            return climbStairs(n-2)+climbStairs(n-1)
+//        }
+//    }递归超时了
+        public int climbStairs(int n) {
+            int[] dp = new int[n + 1];
+            dp[0]=1;
+            dp[1]=1;
+            for (int i = 2; i <= n; i++) {
+                dp[i]=dp[i-2]+dp[i-1];
+            }
+            return dp[n];
+    }
+
+}

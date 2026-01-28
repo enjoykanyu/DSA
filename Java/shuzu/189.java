20edb24aceaa876914408f7c8586e848790d46da
enjoykanyu <creatkanyu@outlook.com>
2026-01-28 22:59:38 +0800

    反转

--- Java/shuzu/189.java
+++ Java/shuzu/189.java
@@ -1,5 +1,18 @@
 class Solution {
     public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - k-1);
+        reverse(nums, nums.length - k, nums.length - 1);
+        reverse(nums, 0, nums.length - 1);
 
+    }
+    public void reverse(int[] nums, int k,int l) {
+        while (k < l) {
+            int temp = nums[k];
+            nums[k] = nums[l];
+            nums[l] = temp;
+            k++;
+            l--;
+        }
     }
 }

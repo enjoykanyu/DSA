dd5af065bd7764f5e1ae20a90a997ac01bee045b
enjoykanyu <creatkanyu@outlook.com>
2026-02-11 23:03:51 +0800

    动态规划解决

--- Java/DP/152.java
+++ Java/DP/152.java
@@ -14,4 +14,22 @@ class Solution {
         }
         return value;
     }
+
+}
+//优化的版本 采用动态规划以右端点
+class Solution {
+    public int maxProduct3(int[] nums) {
+        int dpMax[] = new int[nums.length];
+        int dpMin[] = new int[nums.length];
+
+        dpMax[0] = nums[0];
+        dpMin[0] = nums[0];
+        int value = Integer.MIN_VALUE;
+        for (int i = 1; i <nums.length ; i++) {
+            dpMax[i] = Math.max(Math.max(dpMax[i-1]*nums[i],dpMin[i-1]*nums[i]),nums[i]);
+            dpMin[i] = Math.min(Math.min(dpMax[i-1]*nums[i],dpMin[i-1]*nums[i]),nums[i]);
+            value = Math.max(value,dpMax[i]);
+        }
+        return Math.max(value,nums[0]);
+    }
 }
